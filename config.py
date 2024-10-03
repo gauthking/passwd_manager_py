74757d4039c2b4faa30068918008c83f3dcc4fff
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2024-10-03 23:12:34 +0530

    init commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 MASTER_PWD = "put ur master pwd" 
-SECRET_KEY_PATH = "D:\MainFolders\programcodestuff\python\passwd_manager\secret.key"  
+SECRET_KEY_PATH = ""  
 
 def load_key():
     with open(SECRET_KEY_PATH, 'rb') as key_file:
